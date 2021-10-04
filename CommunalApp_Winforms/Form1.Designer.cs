9bd7a761cfd4f68203e1d2624dd9e9ee4a65a151
Talgat Dosanov <talgat.dosanov@outlook.com>
2021-10-04 16:24:44 +0400

    Добавлены элементы интерфейса

--- CommunalApp_Winforms/Form1.Designer.cs
+++ CommunalApp_Winforms/Form1.Designer.cs
@@ -32,65 +32,202 @@ namespace CommunalApp_Winforms
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorService = new System.Windows.Forms.ErrorProvider(this.components);
             this.maskWaterBox1 = new System.Windows.Forms.MaskedTextBox();
             this.maskWaterVolBox2 = new System.Windows.Forms.MaskedTextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePickerStart1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePickerEnd2 = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxService = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.errorDate = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorService)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorDate)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(40, 29);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(16, 139);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.Size = new System.Drawing.Size(345, 25);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Введите цену 1 куб. м. воды: ";
+            this.label1.Text = "Введите цену  за 1  ед. изм.: ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(40, 129);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 185);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(211, 13);
+            this.label2.Size = new System.Drawing.Size(345, 26);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Введите показания счетчика (куб. метр)";
+            this.label2.Text = "Введите показания счетчика : ";
             // 
-            // errorProvider1
+            // errorService
             // 
-            this.errorProvider1.ContainerControl = this;
+            this.errorService.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorService.ContainerControl = this;
             // 
             // maskWaterBox1
             // 
-            this.maskWaterBox1.Location = new System.Drawing.Point(40, 65);
+            this.maskWaterBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskWaterBox1.Location = new System.Drawing.Point(373, 137);
+            this.maskWaterBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.maskWaterBox1.Mask = "0.00";
             this.maskWaterBox1.Name = "maskWaterBox1";
-            this.maskWaterBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskWaterBox1.Size = new System.Drawing.Size(155, 22);
             this.maskWaterBox1.TabIndex = 4;
+            this.maskWaterBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskWaterBox1_MaskInputRejected);
             // 
             // maskWaterVolBox2
             // 
-            this.maskWaterVolBox2.Location = new System.Drawing.Point(40, 171);
-            this.maskWaterVolBox2.Mask = "0.00";
+            this.maskWaterVolBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.maskWaterVolBox2.Location = new System.Drawing.Point(373, 183);
+            this.maskWaterVolBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.maskWaterVolBox2.Mask = "0000.00";
             this.maskWaterVolBox2.Name = "maskWaterVolBox2";
-            this.maskWaterVolBox2.Size = new System.Drawing.Size(100, 20);
+            this.maskWaterVolBox2.Size = new System.Drawing.Size(155, 22);
             this.maskWaterVolBox2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(577, 118);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(505, 185);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(16, 230);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(345, 28);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Выберите дату  начала расчета: ";
+            // 
+            // dateTimePickerStart1
+            // 
+            this.dateTimePickerStart1.CustomFormat = "";
+            this.dateTimePickerStart1.Location = new System.Drawing.Point(373, 230);
+            this.dateTimePickerStart1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePickerStart1.MinDate = new System.DateTime(2010, 1, 1, 0, 0, 0, 0);
+            this.dateTimePickerStart1.Name = "dateTimePickerStart1";
+            this.dateTimePickerStart1.Size = new System.Drawing.Size(155, 22);
+            this.dateTimePickerStart1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(16, 279);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(345, 28);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Выберите дату конца расчета :";
+            // 
+            // dateTimePickerEnd2
+            // 
+            this.dateTimePickerEnd2.Location = new System.Drawing.Point(373, 278);
+            this.dateTimePickerEnd2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dateTimePickerEnd2.Name = "dateTimePickerEnd2";
+            this.dateTimePickerEnd2.Size = new System.Drawing.Size(155, 22);
+            this.dateTimePickerEnd2.TabIndex = 10;
+            // 
+            // comboBoxService
+            // 
+            this.comboBoxService.FormattingEnabled = true;
+            this.comboBoxService.Location = new System.Drawing.Point(16, 59);
+            this.comboBoxService.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxService.Name = "comboBoxService";
+            this.comboBoxService.Size = new System.Drawing.Size(219, 24);
+            this.comboBoxService.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(16, 23);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Вид расчета";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 348);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 28);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Прошлые показания";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(361, 348);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 28);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Сохранить результат";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // errorDate
+            // 
+            this.errorDate.BlinkStyle = System.Windows.Forms.ErrorBlinkStyle.NeverBlink;
+            this.errorDate.ContainerControl = this;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(207, 348);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 28);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Расчитать";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::CommunalApp_Winforms.Properties.Resources.les_derevia_doroga_205713_3840x2160;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(551, 447);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBoxService);
+            this.Controls.Add(this.dateTimePickerEnd2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePickerStart1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.maskWaterVolBox2);
             this.Controls.Add(this.maskWaterBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Калькулятор Коммунальных услуг";
             this.TopMost = true;
-            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorService)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorDate)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -100,9 +237,20 @@ namespace CommunalApp_Winforms
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorService;
         private System.Windows.Forms.MaskedTextBox maskWaterVolBox2;
         private System.Windows.Forms.MaskedTextBox maskWaterBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEnd2;
+        private System.Windows.Forms.ComboBox comboBoxService;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorDate;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- CommunalApp_Winforms/Form1.cs
+++ CommunalApp_Winforms/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Data;
 using System.Windows.Forms;
 
 namespace CommunalApp_Winforms
@@ -9,22 +10,95 @@ namespace CommunalApp_Winforms
         public Form1()
         {
             InitializeComponent();
-            maskWaterBox1.Validating += waterBox_Validating;
-            maskWaterVolBox2.Validating += waterValueBox_Validating;
+            maskWaterBox1.Validating += maskWaterBox1_Validating;
+            maskWaterVolBox2.Validating += maskWaterVolBox2_Validating;
+            dateTimePickerStart1.Validating += dateTimePickerStart1_Validating;
+            dateTimePickerEnd2.Validating += dateTimePickerEnd2_Validating;
+            //table
+
+            var table = new DataTable();
+            table.Columns.Add("Цена куб.м. воды");
+            table.Columns.Add("Показания счетчика куб. м.");
+            table.Columns.Add("C");
+            table.Columns.Add("По");
+            table.Columns.Add("Итоговая цена в руб.");
+            dataGridView1.DataSource = table;
+
+            // ComboBox settings
+            comboBoxService.DropDownStyle = ComboBoxStyle.DropDown;
+            comboBoxService.Items.AddRange(new string[] { "Расчет водоснабжения",
+                                                    "Расчет электроэнергии",
+                                                    "Расчет газоснабжения"});
+            comboBoxService.SelectedIndexChanged += comboBoxService_SelectedIndexChanged;
+            comboBoxService.SelectedItem = comboBoxService.Items[1];
+
+            //dateTimePicker setting
         }
 
-        private void waterValueBox_Validating(object sender, CancelEventArgs e)
+        private void dateTimePickerEnd2_Validating(object sender, CancelEventArgs e)
         {
-            if(String.IsNullOrEmpty(maskWaterBox1.Text))
+            if (dateTimePickerEnd2.Value <= dateTimePickerStart1.Value)
             {
-                errorProvider1.SetError(maskWaterBox1, "Укажите цену 1 куб. м. воды");
+                errorDate.SetError(dateTimePickerEnd2, "Некорректная дата");
             }
-            
+            else
+            {
+                errorDate.Clear();
+            }
         }
 
-        private void waterBox_Validating(object sender, EventArgs e)
+        private void dateTimePickerStart1_Validating(object sender, CancelEventArgs e)
         {
-            
+            if(dateTimePickerStart1.Value >= dateTimePickerEnd2.Value)
+                {
+                    errorDate.SetError(dateTimePickerStart1, "Некорректная дата");
+                }
+            else
+            {
+                errorDate.Clear();
+            }
+        }
+
+        private void maskWaterVolBox2_Validating(object sender, CancelEventArgs e)
+        {
+            if (!maskWaterVolBox2.MaskCompleted)
+            {
+                errorService.SetError(maskWaterVolBox2, "Значение не может быть пустым");
+            }
+            else
+            {
+                errorService.Clear();
+            }
+        }
+
+        private void maskWaterBox1_Validating(object sender, CancelEventArgs e)
+        {
+            if(!maskWaterBox1.MaskCompleted)
+            {
+                errorService.SetError(maskWaterBox1, "Значение не может быть пустым");
+            }
+        }
+
+        private void comboBoxService_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBoxService.SelectedIndex == 0)
+            {
+                
+            }
+        }
+
+        // Error event ComboBox
+        
+              
+       
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void maskWaterBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
         }
     }
 }
